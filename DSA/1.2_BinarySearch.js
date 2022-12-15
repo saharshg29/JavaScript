2ee1bee2e7b98df2f42f8382444143d8337c3c7f
Saharsh Gupta <saharshg29@gmail.com>
2022-12-15 13:55:22 +0530

    Binary Search completed

--- DSA/1.2_BinarySearch.js
+++ DSA/1.2_BinarySearch.js
@@ -10,21 +10,33 @@
 // key = 6  Output -> False
 // key = 5  Output -> True
 
-function BinarySearch(array, key) {
-  let start = 0;
-  let end = array.length - 1;
-  let middle = Math.floor(start + end) / 2;
-  while (key !== array[middle] && start <= middle) {
-    if (key > array[middle]) {
-      start = middle + 1;
-    } else {
-      end = middle - 1;
-    }
-    middle = Math.floor(start + end) / 2;
-  }
-  return array[middle] === key ? middle : -1;
+
+function binarySearch(arr, l, r, x){
+	if (r >= l) {
+		let mid = l + Math.floor((r - l) / 2);
+
+		// If the element is present at the middle
+		// itself
+		if (arr[mid] == x)
+			return mid;
+
+		// If element is smaller than mid, then
+		// it can only be present in left subarray
+		if (arr[mid] > x)
+			return binarySearch(arr, l, mid - 1, x);
+
+		// Else the element can only be present
+		// in right subarray
+		return binarySearch(arr, mid + 1, r, x);
+	}
+
+	// We reach here when element is not
+	// present in array
+	return -1;
 }
 
-let array = [1, 2, 3, 4, 5];
-let key = 1;
-console.log(BinarySearch(array, key));
+let arr = [ 2, 3, 4, 10, 40 ];
+let x = 10;
+let n = arr.length
+let result = binarySearch(arr, 0, n - 1, x);
+console.log(result)
