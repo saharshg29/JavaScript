937b2bae74789c6295075dbfc55fd46aaaadcb8c
Saharsh Gupta <saharshg29@gmail.com>
2022-12-25 00:02:42 +0530

    Quick Sort javscript

--- DSA/2.6_QuickSort.js
+++ DSA/2.6_QuickSort.js
@@ -18,8 +18,25 @@
 // It will take 3 arguments: array, start index and end index
 // We will grab pivot from start of our array
 
-function pivotUtility(array, start = 0, end = array.length - 1) {
-    let pivotIndex = start;
-    let pivot = array[start];
-    for (let i = 1; i<=)
+function quick_Sort(origArray) {
+	if (origArray.length <= 1) { 
+		return origArray;
+	} else {
+
+		var left = [];
+		var right = [];
+		var newArray = [];
+		var pivot = origArray.pop();
+		var length = origArray.length;
+
+		for (var i = 0; i < length; i++) {
+			if (origArray[i] <= pivot) {
+				left.push(origArray[i]);
+			} else {
+				right.push(origArray[i]);
+			}
+		}
+
+		return newArray.concat(quick_Sort(left), pivot, quick_Sort(right));
+	}
 }
