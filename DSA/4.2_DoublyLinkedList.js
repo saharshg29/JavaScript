1abdbe2c0eb068d09e3759e1ea3b7dd3d010e0f4
Saharsh Gupta <saharshg29@gmail.com>
2023-05-29 17:04:38 +0530

    resunming this journey

--- DSA/4.2_DoublyLinkedList.js
+++ DSA/4.2_DoublyLinkedList.js
@@ -14,4 +14,25 @@ class DoubleLinkedList{
         this.tail = null;
         this.length = 0;
     }
+
+    push(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        }else {
+            this.tail.next = newNode
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
+
+const dll = new DoubleLinkedList()
+
+dll.push("Hello")
+dll.push("world")
+
+console.log(dll)
