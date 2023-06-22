0c082089ed98190c517282ab7218829ed3a5787a
Saharsh Gupta <saharshg29@gmail.com>
2023-06-22 23:07:08 +0530

    printintg all subsequence using recursion

--- Recursion/17.js
+++ Recursion/17.js
@@ -1,18 +1,18 @@
 // Printing Subseqences of a list using recursion
 
 function all(idx, arr, empty = []) {
-  if (idx >= arr.length) {
-    console.log(empty);
-    return;
-  }
-  // console.log(idx);
-  empty.push(arr[idx]);
-  all(idx + 1, arr, empty); // -----------------> Take an element
-  // arr.splice(i, 1)
-  empty.pop();
-  all(idx + 1, arr, empty); // -----------------> Not take element
+    if (idx >= arr.length) {
+       console.log(empty);
+        return 
+    }
+    // console.log(idx);
+    empty.push(arr[idx])
+    all(idx+1, arr, empty) // -----------------> Take an element
+    // arr.splice(i, 1)
+    empty.pop()
+    all(idx+1, arr, empty) // -----------------> Not take element
 }
 
-let arr = [3, 1, 2];
+let arr = [3,1,2]
 
-all(0, arr);
+all(0, arr)
