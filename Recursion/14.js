e9bef50ecfef40bccac6f84963f94c53b4e332f2
Saharsh Gupta <saharshg29@gmail.com>
2023-06-22 18:10:46 +0530

    fibonacci series

--- Recursion/14.js
+++ Recursion/14.js
@@ -7,11 +7,13 @@ let fibonacci = (n) => {
     else if (n == 1|| n == 2) {
         return 1
     }
-    return n + fibonacci(n-1)
+    
+    return fibonacci(n-1) + fibonacci(n-2)
 }
 
-console.log(fibonacci(1));
-console.log(fibonacci(2));
-console.log(fibonacci(3));
-console.log(fibonacci(4));
-console.log(fibonacci(5));
+// console.log(fibonacci(0));
+// console.log(fibonacci(1));
+// console.log(fibonacci(2));
+// console.log(fibonacci(3));
+// console.log(fibonacci(4));
+console.log(fibonacci(10));
